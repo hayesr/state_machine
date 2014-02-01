529013ccc3e16f6ba1ce1d7f9e9a9de2d02209eb
Eric Hayes <eric@erichayes.net>
2014-01-31 16:30:48 -0800

    Make around_validation public

--- lib/state_machine/integrations/active_model.rb
+++ lib/state_machine/integrations/active_model.rb
@@ -397,6 +397,11 @@ module StateMachine
         object.errors.clear if supports_validations?
       end
       
+      # Runs state events around the object's validation process
+      def around_validation(object)
+        object.class.state_machines.transitions(object, action, :after => false).perform { yield }
+      end
+      
       protected
         # Whether observers are supported in the integration.  Only true if
         # ActiveModel::Observer is available.
@@ -509,10 +514,6 @@ module StateMachine
           owner_class.set_callback(:validation, :around, self, :prepend => true)
         end
         
-        # Runs state events around the object's validation process
-        def around_validation(object)
-          object.class.state_machines.transitions(object, action, :after => false).perform { yield }
-        end
         
         # Creates a new callback in the callback chain, always inserting it
         # before the default Observer callbacks that were created after
